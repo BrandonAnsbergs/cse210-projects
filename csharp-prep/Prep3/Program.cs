49ce9468a6003100cfdcbe4f3ac5dea0e1ef1311
Brandon Ansbergs <ans22004@byui.edu>
2024-01-17 20:37:56 -0700

    chore 3-5 prep programs complete.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Xml.Schema;
 
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); 
+        int guess = -1;
+        int countGuess = 0;
+
+        Console.WriteLine("Guess the Magic Number!");
+        
+        while (guess != magicNumber)
+        {
+            Console.WriteLine("What is your guess?");
+            guess = int.Parse(Console.ReadLine());
+
+            countGuess++;
+
+            string guessans; 
+
+            if (guess > magicNumber)
+            {
+                guessans = "Lower";
+            }
+            
+            else if (guess < magicNumber)
+            {
+                guessans = "Higher";
+            }
+
+            else 
+            {
+                guessans = "You guessed it!";
+            }
+
+            Console.WriteLine($"{guessans}");
+        }
+            Console.WriteLine($"It took you {countGuess} tries to guess the Magic Number.");
     }
 }
